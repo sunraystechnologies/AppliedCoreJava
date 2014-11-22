595d770c6ed0b70a740cb02ea712033ce2c10c0b
sunraystechnologies <hrd@sunrays.co.in>
2014-11-22 18:46:35 +0530

    OOP cahnges

--- src/com/sunrays/oop/TestPerson.java
+++ src/com/sunrays/oop/TestPerson.java
@@ -26,32 +26,29 @@ public class TestPerson {
 
 		System.out.println("__________________________________ ");
 
-		/*
-		 * System.out.println("$$Calling Employee Class 3 Param Constructor");
-		 * 
-		 * Employee e = new Employee("Nakul", "Shukla", "CEO");
-		 * 
-		 * System.out.println(" First Name " + e.getFirstName());
-		 * 
-		 * System.out.println(" Last Name " + e.getLastName());
-		 * 
-		 * System.out.println(" Designation " + e.getDesignaiton());
-		 * 
-		 * System.out.println("__________________________________ "); //Test
-		 * override method
-		 * 
-		 * 
-		 * System.out.println("$$Calling Person Class default Constructor");
-		 * 
-		 * Person p1 = new Person();
-		 * 
-		 * 
-		 * System.out.println("\n\n$$Calling Employee Class default Constructor")
-		 * ; Person p2 = new Employee();
-		 * 
-		 * p1.changeAddress(); p2.changeAddress();
-		 */
+		System.out.println("$$Calling Employee Class 3 Param Constructor");
+
+		Employee e = new Employee("Nakul", "Shukla", "CEO");
+
+		System.out.println(" First Name " + e.getFirstName());
+
+		System.out.println(" Last Name " + e.getLastName());
+
+		System.out.println(" Designation " + e.getDesignaiton());
+
+		System.out.println("__________________________________ ");
+
+		// Test overridden method
+
+		System.out.println("$$Calling Person Class default Constructor");
+
+		Person p1 = new Person();
+
+		System.out.println("\n\n$$Calling Employee Class default Constructor");
+		Person p2 = new Employee();
+
+		p1.changeAddress();
+		p2.changeAddress();
 
 	}
-
 }
